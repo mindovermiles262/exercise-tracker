b36bff1005b504363dce36b52d35e9270fd3ff35
Erik Johnson <mindovermiles262@gmail.com>
2017-07-01 14:06:00 -0700

    minor navbar colors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,11 +28,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "User Profile Successfully Updated"
       redirect_to @user
