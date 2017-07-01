223b7196a4203cb09fe6ad96cd97e616be6b5f69
Erik Johnson <mindovermiles262@gmail.com>
2017-07-01 15:31:52 -0700

    remember users via cookies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701025541) do
+ActiveRecord::Schema.define(version: 20170701214506) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170701025541) do
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
   end
 
 end
