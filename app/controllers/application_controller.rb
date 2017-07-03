90b61be87dba2814a585717990d89c5b8b0f8e90
Andy Duss <mindovermiles262@gmail.com>
2017-07-03 07:30:22 -0700

    disable view other users posts in profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  include UsersHelper
+
+private
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please Log In"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def 
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,14 +10,5 @@ class PostsController < ApplicationController
   
   def create
   end
-
-private
-
-  def logged_in_user
-    unless logged_in?
-      flash[:danger] = "Please Log In"
-      redirect_to login_url
-    end
-  end
   
 end
