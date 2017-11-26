dbf1650256b44a954915a80d8054db33fd57e1c1
Andy Duss <mindovermiles262@gmail.com>
2017-11-25 19:26:30 -0800

    add subfolder config

--- config.ru
+++ config.ru
@@ -2,4 +2,6 @@
 
 require_relative 'config/environment'
 
-run Rails.application
+map '/exercise' do
+  run Rails.application
+end
--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,10 @@ module ExerciseTracker
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Set to run in /exercise subfolder
+    config.root_directory = '/exercise/'
+    config.action_controller.relative_url_root = '/exercise'
+    config.relative_url_root = '/exercise'
   end
 end
